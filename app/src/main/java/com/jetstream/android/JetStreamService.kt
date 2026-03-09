76d523d82c676a10744390b4f6da13250e6c59b6
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2026-03-09 12:00:59 +0530

    added basic binder setup and persistent notification
    Signed-off-by: Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>

---
+++ app/src/main/java/com/jetstream/android/JetStreamService.kt
@@ -0,0 +1,63 @@
+package com.jetstream.android
+
+import android.app.*
+import android.content.ClipDescription
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class JetStreamService: Service() {
+
+    // Binder setup
+    inner class LocalBinder : Binder() { fun getService() = this@JetStreamService }
+    override fun onBind(intent: Intent) = LocalBinder()
+
+    private val CHANNEL_ID = "JetStreamService"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Create notification channel
+        val serviceChannel = NotificationChannel(
+            CHANNEL_ID,
+            "Connectivity Service",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(serviceChannel)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        // Create notification
+        val notification = buildNotification("Disconnected")
+
+        // Start foreground service with the notification
+        startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+
+        return START_STICKY
+    }
+
+    fun buildNotification(description: String): Notification {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("JetStream")
+            .setContentText(description)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+    }
+
+    fun wsConnect() {
+        println("Connected")
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.notify(1, buildNotification("Connected"))
+    }
+
+    fun wsDisconnect() {
+        println("Disconnected")
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.notify(1, buildNotification("Connected"))
+    }
+}
--- app/src/main/java/com/jetstream/android/MainActivity.kt
+++ app/src/main/java/com/jetstream/android/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.jetstream.android
 
+import android.Manifest
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,23 +26,45 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.jetstream.android.ui.theme.JetStreamTheme
 
 class MainActivity : ComponentActivity() {
+    private var runningService by mutableStateOf<JetStreamService?>(null)
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            runningService = (service as JetStreamService.LocalBinder).getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            runningService = null
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Start service as soon as the app is opened
+        val intent = Intent(this, JetStreamService::class.java)
+        startForegroundService(intent)
+
+        bindService(Intent(this, JetStreamService::class.java), connection, Context.BIND_AUTO_CREATE)
+
         enableEdgeToEdge()
         setContent {
             JetStreamTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(modifier = Modifier.padding(innerPadding))
+                    MainScreen(modifier = Modifier.padding(innerPadding), runningService)
                 }
             }
         }
@@ -41,8 +72,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier, runningService: JetStreamService? = null) {
     var text by remember { mutableStateOf("") }
+
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { }
+    LaunchedEffect(Unit) {
+        launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
 
     Column(
         modifier = modifier
@@ -73,7 +109,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Button(
-                onClick = { },
+                onClick = {
+                    runningService?.wsConnect()
+                },
                 modifier = Modifier.weight(1f),
                 enabled = text.isNotEmpty()
             ) {
@@ -81,7 +119,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
             }
 
             Button(
-                onClick = { },
+                onClick = {
+                    runningService?.wsDisconnect()
+                },
                 modifier = Modifier.weight(1f),
                 enabled = text.isNotEmpty()
             ) {
